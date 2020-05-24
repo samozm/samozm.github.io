7ae7a3d0e381751dc080db09a784aefd928676cf
samozm <samoz@umich.edu>
2020-05-23 21:40:53 -0500

    new form

--- js/contact_me.js
+++ js/contact_me.js
@@ -17,6 +17,7 @@ class PersonalContact extends React.Component {
                 action="https://formspree.io/xpzypzqk"
                 method="POST"
             >
+
                 <label>Name:</label>
                 <input type="text" name="name" />
                 <label>Email:</label>
@@ -26,6 +27,45 @@ class PersonalContact extends React.Component {
                 {status === "SUCCESS" ? <p>Thanks!</p> : <button>Submit</button>}
                 {status === "ERROR"  && <p>There appears to be a problem with my mail server right now, please try again later. </p>}
             </form>
+
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <form name="sentMessage" id="contactForm" onSubmit={this.submitForm} action="https://formspree.io/xpzypzqk" method="POST">
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Name</label>
+                                <input type="text" class="form-control" placeholder="Name" id="name" required data-validation-required-message="Please enter your name.">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Email Address</label>
+                                <input type="email" class="form-control" placeholder="Email Address" id="email" required data-validation-required-message="Please enter your email address.">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Phone Number</label>
+                                <input type="tel" class="form-control" placeholder="Phone Number" id="phone" required data-validation-required-message="Please enter your phone number.">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Message</label>
+                                <textarea rows="5" class="form-control" placeholder="Message" id="message" required data-validation-required-message="Please enter a message."></textarea>
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <br>
+                        <div id="success"></div>
+                        {status === "SUCCESS" ? <p>Thanks!</p> : <button>Submit</button>}
+                        {status === "ERROR"  && <p>There appears to be a problem with my mail server right now, please try again later. </p>}
+                    </form>
+                </div>
+            </div>
         );
 
     }
